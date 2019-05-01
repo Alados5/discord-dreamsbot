e8c7700cb45219f032d88eac72198963ba164532
Alados5 <adria.alados@gmail.com>
2019-05-01 17:09:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,26 @@ client.on('message', msg => {
   if(msg.author.bot) return;
   
   var lowtext = msg.content.toLowerCase();
+    
+  //Returns if message doesn't start with prefix
+  if(!msg.content.startsWith(prefix)) return;
+    
+  //Handles arguments to just take the first word
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase(); 
+    
+  if (command == 'aura') {
+    const userx = msg.author;
+        
+    var rolename = args[0];
+    if (!rolename) return msg.reply("You didn't put a role in there!")
+        
+    var therole = msg.guild.roles.find("name", rolename);
+    if (!therole) return msg.reply("This role does not exist")
+        
+    userx.addRole(therole);
+  }
+  
   
 });
 
