d49af8b969dcdad9804e91eecc35abfbd9ce8199
Alados5 <adria.alados@gmail.com>
2020-02-02 17:39:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,15 @@ client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'general');
   if (!channel) return;
   if (channel.id = "530381279749865484") {
-      channel.send(`¡Muy buenas, ${member}! ¡Te damos la bienvenida al Servidor de Dreams en Español!`);
+      channel.send(`¡Muy buenas, ${member}! ¡Te damos la bienvenida al Servidor de Dreams en Español!`+
+                   `\n A modo de presentación, y para romper el hielo, `+
+                   `normalmente pedimos a los nuevos qué faceta de Dreams les gusta más, `+
+                   `y qué están haciendo o quieren hacer en el juego.`+
+                   `\n No dudes en compartir tus creaciones, o pedir ayuda si te hace falta! :D`+
+                   `\n Échale un ojo a las normas del servidor, seguro que pronto un humano hablará contigo`+
+                   `para darte una bienvenida mejor que la que te puedo dar yo.`+
+                   `\n Pero no dudes en usarme para lo que necesites!`+
+                   `\n \n *Beep boop, yo soy un bot creado por Alados5*`);
   }
 });
 
