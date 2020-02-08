968a696319cddbbc2b3c6a41b47a6bc5775f0921
Alados5 <adria.alados@gmail.com>
2020-02-08 18:42:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var prefix = '!';
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'general');
   if (!channel) return;
-  if (channel.id == 530381279749865484 || channel.id == 402992536148377602) {
+  if (channel.id == 530381279749865484) {
     var msgcolor = 8388863;
     var msgtitle = "¡Bienvenid@ al Servidor de Dreams en Español!";
     var desctext = "A modo de presentación, y para romper el hielo, "+
@@ -17,6 +17,7 @@ client.on('guildMemberAdd', member => {
                    "\n\nÉchale un ojo a las normas del servidor, "+
                    "seguro que pronto un humano hablará contigo para darte una bienvenida mejor que la que te puedo dar yo."+
                    "\n\n¡Pero no dudes en usarme para lo que necesites!";
+    /*
     channel.send("¡Muy buenas, " + member + "!", {embed: {
       color: msgcolor,
       description: desctext,
@@ -32,8 +33,9 @@ client.on('guildMemberAdd', member => {
         "text": "Beep boop, yo soy un bot creado por Alados5"
       }
     }})
+    */
     
-    /*
+    //
     channel.send(`¡Muy buenas, ${member}! ¡Te damos la bienvenida al Servidor de Dreams en Español!`+
                  `\nA modo de presentación, y para romper el hielo, `+
                  `normalmente pedimos a los nuevos qué faceta de Dreams les gusta más, `+
@@ -43,7 +45,7 @@ client.on('guildMemberAdd', member => {
                  `para darte una bienvenida mejor que la que te puedo dar yo.`+
                  `\n¡Pero no dudes en usarme para lo que necesites!`+
                  `\n \n    ***Beep boop, yo soy un bot creado por Alados5***`);
-    */
+    //*/
   }
 });
 
