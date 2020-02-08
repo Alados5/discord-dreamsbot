86845f9b8e07c62ee40a25950cd8ecab971dbb53
Alados5 <adria.alados@gmail.com>
2020-02-08 17:37:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('guildMemberAdd', member => {
       description: desctext,
       title: msgtitle,
       thumbnail: {
-        "url": member.avatar
+        "url": "https://cdn.discordapp.com/avatars/" +member.id+ "/" +member.avatar+ ".png";
       },
       image: {
         "url": "https://i.imgur.com/pdmBuaV.png"
