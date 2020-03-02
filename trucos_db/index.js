8357f46e1116eb2f76a8fe3bdbcac8480e579d43
Alados5 <adria.alados@gmail.com>
2020-03-02 01:46:38 +0100

    Create index.js

---
+++ trucos_db/index.js
@@ -0,0 +1,16 @@
+var dbindex = {
+  
+  1: {
+    name: "Centrar Imp",
+    tags: ["Básico", "Función", "Meme"],
+    desc: "Recuerda que puedes mantener pulsado el botón OPTIONS para centrar el duende."
+  },
+  2: {
+    name: "Dormilones",
+    tags: ["Básico", "Spam", "Meme"],
+    desc: "Suscríbete a Dormilones."
+  }
+
+};
+
+module.exports.dbindex = dbindex;
