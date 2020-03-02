0b664fced27b8108a812046d1991381f35318236
Alados5 <adria.alados@gmail.com>
2020-03-02 02:15:47 +0100

    Update index.js

--- trucos_db/index.js
+++ trucos_db/index.js
@@ -3,12 +3,39 @@ var dbindex = {
   1: {
     name: "Centrar Imp",
     tags: ["Básico", "Función", "Meme"],
-    desc: "Recuerda que puedes mantener pulsado el botón OPTIONS para centrar el duende."
+    desc: "Recuerda que puedes mantener pulsado el botón OPTIONS para centrar el duende.",
+    user: "Media Molecule",
+    link: ""
   },
   2: {
     name: "Dormilones",
     tags: ["Básico", "Spam", "Meme"],
-    desc: "Suscríbete a Dormilones."
+    desc: "Suscríbete a Dormilones.",
+    user: "Carloitaben",
+    link: "https://www.youtube.com/c/dormilonesdreams"
+  },
+  3: {
+    name: "Pintura: Spray vs Tinte",
+    tags: ["Básico", "Esculpir", "Pintura", "Spray", "Tinte"],
+    desc: "Hay dos maneras de aplicar color a una escultura: \n"+
+          "La primera es pintar con el spray en el modo esculpir, "+
+          "que permite pintar de forma precisa regiones concretas de las esculturas. \n"+
+          "La segunda es, fuera del modo esculpir, abrir el menú de la escultura con L1+Cuadrado, "+
+          "escoger un color y subir el slider de tinte, cosa que sobreescribirá el color de la escultura "+
+          "y hará que toda sea uniforme, del color seleccionado. \n"+
+          "Clonar una escultura y cambiarle el color a una con el slider no afecta al termo de gráficos, "+
+          "en cambio, entrar en el modo esculpir y pintar con el spray sí!",
+    user: "Alados5",
+    link: ""
+  },
+  4: {
+    name: "El spray no cambia el color!",
+    tags: ["Básico", "Color", "Esculpir", "Spray", "Pintura", "Error"],
+    desc: "Si ves que al aplicar Spray en el modo esculpir no cambia el color de tu escultura, "+
+          "seguramente tengas el slider de tinte a más del 100%. Sal del modo esculpir, "+
+          "reduce su valor, y prueba de nuevo. Para más detalles consulta el truco 3.",
+    user: "Alados5",
+    link: ""
   }
 
 };
