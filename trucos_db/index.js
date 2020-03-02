6f66b153f0ab45371cd64121218cf4692effc1d8
Alados5 <adria.alados@gmail.com>
2020-03-02 02:25:20 +0100

    Update index.js

--- trucos_db/index.js
+++ trucos_db/index.js
@@ -2,21 +2,21 @@ var dbindex = {
   
   1: {
     name: "Centrar Imp",
-    tags: ["Básico", "Función", "Meme"],
+    tags: ["básico", "función", "meme"],
     desc: "Recuerda que puedes mantener pulsado el botón OPTIONS para centrar el duende.",
     user: "Media Molecule",
     link: ""
   },
   2: {
     name: "Dormilones",
-    tags: ["Básico", "Spam", "Meme"],
+    tags: ["básico", "spam", "meme"],
     desc: "Suscríbete a Dormilones.",
     user: "Carloitaben",
     link: "https://www.youtube.com/c/dormilonesdreams"
   },
   3: {
     name: "Pintura: Spray vs Tinte",
-    tags: ["Básico", "Esculpir", "Pintura", "Spray", "Tinte"],
+    tags: ["básico", "esculpir", "pintura", "spray", "tinte"],
     desc: "Hay dos maneras de aplicar color a una escultura: \n"+
           "La primera es pintar con el spray en el modo esculpir, "+
           "que permite pintar de forma precisa regiones concretas de las esculturas. \n"+
@@ -30,7 +30,7 @@ var dbindex = {
   },
   4: {
     name: "El spray no cambia el color!",
-    tags: ["Básico", "Color", "Esculpir", "Spray", "Pintura", "Error"],
+    tags: ["básico", "color", "esculpir", "spray", "pintura", "error"],
     desc: "Si ves que al aplicar Spray en el modo esculpir no cambia el color de tu escultura, "+
           "seguramente tengas el slider de tinte a más del 100%. Sal del modo esculpir, "+
           "reduce su valor, y prueba de nuevo. Para más detalles consulta el truco 3.",
