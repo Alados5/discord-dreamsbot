443c4ff3eb3134ba67f5a30d132f14057c224978
Alados5 <adria.alados@gmail.com>
2020-03-03 20:43:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,8 @@ client.on('message', msg => {
     var askedname = msg.content.slice(prefix.length+command.length+1);
     var server = msg.guild;
     var chname = "px_" + askedname;
-    server.createChannel(chname, {type:"text"});//, parent:"567722499052404756"});
+    server.channels.create(chname, {type:"text"});//, parent:"567722499052404756"});
+    msg.channel.send("Ok I guess");
   }
   // END CREATE CHANNEL
   
