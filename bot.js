68598348bdb5db1a12afaa8dc0452e72f5f8cb16
Alados5 <adria.alados@gmail.com>
2020-03-12 00:45:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -551,9 +551,13 @@ client.on('message', msg => {
       var debugch = msg.guild.channels.find('id','684539074224455763');
       msg.channel.send("¿Seguro? ¡Esta acción es irreversible!\n"+
                        "Reacciona con el tick en menos de 10 segundos para confirmar.").then(sentmsg => {
-        sentmsg.react('✅');
-        const filter = (reaction, user) => reaction.emoji.name === '✅' && (user.roles.has(projrole) || (user.permissions.has('ADMINISTRATOR') && user.id != 573146997419278336));
-        sentmsg.awaitReactions(filter, { time: 10000 }).then(sentmsg.channel.send("OK"));
+        sentmsg.react('✅').then(tickreaction => {
+          const filter = (reaction, user) => reaction.emoji.name === '✅' && (user.roles.has(projrole) || user.permissions.has('ADMINISTRATOR'));
+          sentmsg.awaitReactions(filter, { time: 10000 }).then(sentmsg.channel.send("OK"));
+
+          
+        });
+        
           //.then(reactcol => debugch.send('```prolog\nPROYECTO "'+sentmsg.channel.name.toUpperCase()+'" ELIMINADO\n```'))
           //.catch(sentmsg.channel.send("No se ha confirmado."))
       });
