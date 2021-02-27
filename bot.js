7a7368cdf9c7be5c5a3660e9fe5f0df9fefb460f
Alados5 <adria.alados@gmail.com>
2021-02-27 16:43:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,19 @@ client.login(process.env.BOT_TOKEN);
 var prefix = '!';
 
 client.on('message', (msg) => {
+  
+  // Returns if author is a bot
   if(msg.author.bot) return;
+
+  var lowtext = msg.content.toLowerCase();
+  
+  // ID of channel: #bot_debug
+  var debugch = msg.guild.channels.find('id','688107638239920282');
+  
+  // Returns if message doesn't start with prefix
+  if(!msg.content.startsWith(prefix)) {
+    return;
+  }
+  
   msg.channel.send('Debugging');
 });
