026d42bba4ec0444819d4ea481a86a212fdafc54
Alados5 <adria.alados@gmail.com>
2021-03-01 03:50:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
 // CLIENT:
 const Discord = require('discord.js');
-const client = new Discord.Client({ ws: { intents: ['GUILD_MEMBERS', 'GUILD_PRESENCES'] } });
+const client = new Discord.Client({ ws: { intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_EMOJIS', 'GUILD_INTEGRATIONS',
+                                                    'GUILD_WEBHOOKS', 'GUILD_INVITES', 'GUILD_VOICE_STATES', 'GUILD_PRESENCES',
+                                                    'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MESSAGE_TYPING',
+                                                    'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'DIRECT_MESSAGE_TYPING'] } });
 
 // THIS MUST BE THIS WAY:
 client.login(process.env.BOT_TOKEN);
